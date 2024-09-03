4cf4dc17d7d7ccde182bb4425e00291efbb0373d
Dragosh51 <dragosh.bindila@gmail.com>
2024-09-03 11:03:36 +0100

    the user also receives an email when the send message button is clicked

--- iPortfolio/forms/send_email.php
+++ iPortfolio/forms/send_email.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
-    // Create a new PHPMailer instance
+    // Create a new PHPMailer instance for sending email to you
     $mail = new PHPMailer(true);
 
     try {
@@ -37,7 +37,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->AltBody = "Name: $name\nEmail: $email\nMessage: $message";
 
         $mail->send();
-        echo 'Message has been sent';
+
+        // Send auto-reply to the user
+        $autoReply = new PHPMailer(true);
+        $autoReply->isSMTP();
+        $autoReply->Host = 'smtp.gmail.com';
+        $autoReply->SMTPAuth = true;
+        $autoReply->Username = 'dragosh.bindila@gmail.com'; // Your Gmail address
+        $autoReply->Password = 'jyvi kvwy osrz oevb'; // The 16-digit app password from Google
+        $autoReply->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $autoReply->Port = 587;
+
+        // Recipients for auto-reply
+        $autoReply->setFrom('dragosh.bindila@gmail.com', 'Dragos Bindila'); // Your email and name
+        $autoReply->addAddress($email, $name); // User's email
+
+        // Auto-reply content
+        $autoReply->isHTML(true);
+        $autoReply->Subject = 'Thank you for contacting me';
+        $autoReply->Body = '<p>Hello!</p><p>Thank you so much for wanting to get in contact with me!</p><p>This is an automatic reply but I will get back to you as soon as I can!</p><p>Kind regards,<br>Dragos</p>';
+        $autoReply->AltBody = "Hello!\n\nThank you so much for wanting to get in contact with me!\nThis is an automatic reply but I will get back to you as soon as I can!\n\nKind regards,\nDragos";
+
+        $autoReply->send();
+
+        echo 'Message has been sent and an auto-reply has been sent to the user.';
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
