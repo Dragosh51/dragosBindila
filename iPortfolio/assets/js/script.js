24a8381d5fb0258f04d5fde78f2af474a39b0450
Dragosh51 <dragosh.bindila@gmail.com>
2024-09-03 10:06:34 +0100

    added new logos and made them resposnsive, updated style for contact form

--- iPortfolio/assets/js/script.js
+++ iPortfolio/assets/js/script.js
@@ -1,31 +1,49 @@
-$(document).ready(function() {
-    $('#submit-button').on('click', function(e) {
-      e.preventDefault();
-  
-      // Gather form data
-      const name = $('#name-field').val();
-      const email = $('#email-field').val();
-      const subject = $('#subject-field').val();
-      const message = $('#message-field').val();
-  
-      // AJAX request
-      $.ajax({
-        url: 'forms/send_email.php', // Path to your PHP file
-        type: 'POST',
-        data: {
-          name: name,
-          email: email,
-          subject: subject,
-          message: message
-        },
-        success: function(response) {
-          $('.sent-message').text(response).show(); // Show success message
-          $('.error-message').hide();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          $('.error-message').text('An error occurred. Please try again.').show(); // Show error message
-          $('.sent-message').hide();
-        }
-      });
+$(document).ready(function () {
+  $('#submit-button').on('click', function (e) {
+    e.preventDefault();
+
+    // Gather form data
+    const name = $('#name-field').val();
+    const email = $('#email-field').val();
+    const subject = $('#subject-field').val();
+    const message = $('#message-field').val();
+
+    // Hide any previous messages
+    $('.error-message').hide();
+    $('.sent-message').hide();
+
+    // Show loading message
+    $('.loading').show();
+
+    // AJAX request
+    $.ajax({
+      url: 'forms/send_email.php', // Path to your PHP file
+      type: 'POST',
+      data: {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+      },
+      success: function (response) {
+        // Hide loading message
+        $('.loading').hide();
+
+        // Show success message
+        $('.sent-message').text(response).show();
+
+        // Hide success message after 1 second
+        setTimeout(function () {
+          $('.sent-message').fadeOut();
+        }, 1000);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        // Hide loading message
+        $('.loading').hide();
+
+        // Show error message
+        $('.error-message').text('An error occurred. Please try again.').show();
+      }
     });
   });
+});
